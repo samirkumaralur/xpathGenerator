b077878a4cd9f87f7628309b61fe73fe7dd0cbb7
samir <samiralurhp@gmail.com>
2020-09-21 22:09:49 +0530

    added xpath library

--- background.js
+++ background.js
@@ -1,7 +1,15 @@
 chrome.contextMenus.create({
-	title:'Get_Sentiment',
-	contexts:['selection'],
-	onclick:function(info,tab){
-		chrome.tabs.sendMessage(tab.id, info.selectionText);
-	}
+  title: "Get_XPath",
+  contexts: [
+    "page",
+    "link",
+    "editable",
+    "image",
+    "video",
+    "audio",
+    "selection",
+  ],
+  onclick: function (info, tab) {
+    chrome.tabs.sendMessage(tab.id, "GetXpath");
+  },
 });
--- myscript.js
+++ myscript.js
@@ -1,14 +1,9 @@
-document.addEventListener('click', function(){
-	console.log(document.URL);
+var selectedElement;
+document.addEventListener("contextmenu", function (event) {
+  selectedElement = event.target;
 });
 
-chrome.runtime.onMessage.addListener(function(message,sender, sendResponse){
-	//alert(message)
-	$.ajax({url:"http://127.0.0.1:5000/get_message",
-		type:"POST",
-		data:{
-			messageSent: JSON.stringify(message)
-		},
-		success:function(result){console.log(result)}
-	});
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log(selectedElement);
+  console.log($(selectedElement).attr("class"));
 });
